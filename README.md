680e18affe52af9f50a449e94b56c494d3099a3a
Julia Najafi <Julia.Najafi@gmail.com>
2016-06-09 10:29:11 -0400

    added directions plus css styling

--- README.md
+++
@@ -1,6 +0,0 @@
-what I still need to do:
-  
-  IF WIN WHAT TO DO (NEED TO TRASNALRE TO ALL)
-
-  timer
-    right now when timer runs out, my movie function is not picking up on my next button
--- trivia.js
+++ trivia.js
@@ -4,58 +4,42 @@ $(document).ready(function(){
     movies: [
       {question: "(1) 'I love the smell of napalm in the morning'", answer: "apocalypse now" },
       {question: "(2) 'A boy's best friend is his mother'", answer: "psycho" },
+      {question: "(3) 'I ate his liver with some fava beans and a nice Chianti!'", answer: "the silence of the lambs"},
+      {question: "(4) 'Here's looking at you, kid'", answer: "casablanca"},
+      {question: "(5) 'You had me at hello'", answer: "jerry maguire"},
+      {question: "(6) 'The greatest trick the devil ever pulled was convincing the world he didn't exist'", answer: "the usual suspects"},
+      {question: "(7) 'As if!'", answer: "clueless"},
+      {question: "(8) 'Oh, he was a little guy... Kinda funny lookin'.'", answer: "fargo"},
+      {question: "(9) 'I have nipples, Greg. Could you milk me?'", answer: "meet the parents"},
+      {question: "(10) 'I'm pretty sure there's a lot more to life than being really, really good looking. And I plan on finding out what that is'", answer: "zoolander"},
     ],
     geography: [
       {question: "(1) In which country is the Blarney Stone?", answer: "ireland"},
       {question: "(2) The city of Cairo in Egypt is generally accepted to have the oldest of what type of institution in the world?", answer: "university"},
+      {question: "(3) Which is the least populated state in the USA?", answer: "wyoming" },
+      {question: "(4) Which capital city has the highest population?", answer: "tokyo"},
+      {question: "(5) What is the largest country in the world?", answer: "russia"},
+      {question: "(6) Half of the world's population lives near what body of water?", answer: "pacific ocean"},
+      {question: "(7) What is the world's only city located in two continents?", answer: "istanbul" },
+      {question: "(8) Which lake, the world's deepest, contains 1/5 of all the world's fresh water?", answer: "lake baikal"},
+      {question: "(9) Which city hosts the headquarters of the EU?", answer: "brussels"},
+      {question: "(10) From which modern-day country did the religion Zoroastrianism originate from?", answer: "iran"},
     ],
     current: [
       {question: "(1) What does ISIL stand for?", answer: "islamic state of iraq and the levant"},
       {question: "(2) Who is the Green Party candidate for the upcoming U.S. presidential election?", answer:"jill stein"},
+      {question: "(3) Which current member of the EU is debating an exit?", answer:"united kingdom"},
+      {question: "(4) What is the popular term that refers to the complex web of censorship and surveillance governing the Chinese internet?", answer: "the great firewall"},
+      {question: "(5) Who is Obama's Supreme Court nominee?", answer: "merrick garland"},
+      {question: "(6) What is the name of the leaked documents that has ensnared some of the world's most prominent leaders, including Iceland's Prime Minister?", answer: "the panama papers"},
+      {question: "(7) What is the name of the virus that is rapidly spreading through South and Central America?", answer: "zika virus"},
+      {question: "(8) Which celebrity recently died of an overdose?", answer: "prince"},
+      {question: "(9) Which country's sovereign wealth fund just invested in Uber?", answer: "saudi arabia"},
+      {question: "(10) What is the name of the billionaire who was revealed to be funding lawsuits against gossip site Gawker?", answer: "peter thiel"},
     ]
   }
 
-//arrays for movie questions and answers
-  var movieQuestions = ["(1) 'I love the smell of napalm in the morning'",
-  "(2) 'A boy's best friend is his mother'",
-  "(3) 'I ate his liver with some fava beans and a nice Chianti!'",
-  "(4) 'Here's looking at you, kid'",
-  "(5) 'You had me at hello'",
-  "(6) 'The greatest trick the devil ever pulled was convincing the world he didn't exist'",
-  "(7) 'As if!'",
-  "(8) 'Oh, he was a little guy... Kinda funny lookin'.'",
-  "(9) 'I have nipples, Greg. Could you milk me?'",
-  "(10) 'I'm pretty sure there's a lot more to life than being really, really good looking. And I plan on finding out what that is'"];
-  var movieAnswers = ["Apocalypse Now", "Psycho", "The Silence of the Lambs", "Casablanca",
-  "Jerry Maguire", "The Usual Suspects", "Clueless", "Fargo", "Meet the Parents",
-  "Zoolander"];
 
-  var geoQuestions = ["(1) In which country is the Blarney Stone?",
-  "(2) The city of Cairo in Egypt is generally accepted to have the oldest of what type of institution in the world?",
-  "(3) Which is the least populated state in the USA?",
-  "(4) Which capital city has the highest population?",
-  "(5) What is the largest country in the world?",
-  "(6) Half of the world's population lives near what body of water?",
-  "(7) What is the world's only city located in two continents?",
-  "(8) Which lake, the world's deepest, contains 1/5 of all the world's fresh water?",
-  "(9) Which city hosts the headquarters of the EU?",
-  "(10) From which country did the religion Zoroastrianism originate from?"];
-  var geoAnswers = ["Ireland", "University", "Wyoming", "Tokyo", "Russia", "Pacific Ocean",
-  "Istanbul", "Lake Baikal", "Brussels", "Iran"];
-
-  var newsQuestions = ["(1) What does ISIL stand for?",
-  "(2) Who is the Green Party candidate for the upcoming presidential election?",
-  "(3) Which current member of the EU is debating an exit?",
-  "(4) What is the popular term that refers to the complex web of censorship and surveillance governing the Chinese internet?",
-  "(5) Who is Obama's Supreme Court nominee?",
-  "(6) What is the name of the leaked documents that has ensnared some of the world's most prominent leaders, including Iceland's Prime Minister?",
-  "(7) What is the name of the virus that is rapidly spreading through South and Central America?",
-  "(8) Which celebrity recently died of an overdose?",
-  "(9) Which country's sovereign wealth fund just invested in Uber?",
-  "(10) What is the name of the billionaire who was revealed to be funding lawsuits against gossip site Gawker?"]
-  var newsAnswers = ["Islamic State of Iraq and the Levant", "Jill Stein", "UK",
-  "The Great Firewall", "Merrick Garland", "The Panama Papers", "Zika", "Prince",
-  "Saudi Arabia", "Peter Thiel"]
 
   // refers to all category buttons generally
   var categoryButton = $(".categories button");
@@ -73,10 +57,36 @@ $(document).ready(function(){
   //start the timer
   //categoryButton.on("click", startTimer);
 
+  $("button#movies").on("mouseover", function(){
+    $("p2#directionMovie").css("visibility", "visible");
+  });
+  $("button#movies").on("mouseleave", function(){
+    $("p2#directionMovie").css("visibility", "hidden");
+  })
+
+  $("button#geography").on("mouseover", function(){
+    $("p2#directionGeo").css("visibility", "visible");
+  });
+  $("button#geography").on("mouseleave", function(){
+    $("p2#directionGeo").css("visibility", "hidden");
+  });
+
+  $("button#current").on("mouseover", function(){
+    $("p2#directionCurrent").css("visibility", "visible");
+  });
+  $("button#current").on("mouseleave", function(){
+    $("p2#directionCurrent").css("visibility", "hidden");
+  });
+
+
+
 
   $(".categories button").on("click", categoryClick);
 
+
   function categoryClick (){
+    $("div.question").css("visibility", "visible");
+    $("#container").css("background-color", "rgb(244,52,64)");
     var category = $(this).attr("id");
     $(".question").empty();
     appendQuestion(category);
@@ -128,7 +138,9 @@ $(document).ready(function(){
           $(".question").append("<h2>You have reached the end of this quiz!</p>");
           $(".question").append("<p2> Your Score: "+ userScore + "/10</p2>");
           i= 0;
+          $("#container").css("background-color", "green");
           userScore= 0;
+          stopTimer();
         }
       }
       else {
@@ -155,8 +167,7 @@ $(document).ready(function(){
     $(".question").append("<div class='scoreTime' id='scoreBox'>"+userScore+"/10"+"</div>")
     $(".question").append("<div class='scoreTime' id='time'>30 s</div>");
     $(".question").append("<p></p>");
-    $(".question").append("<p></p>");
-    $(".question").append("<h2>Directions: name the movie from the quote provided</h2>");
+    //$(".question").append("<h2>Directions: name the movie from the quote provided</h2>");
     $(".question").append("<p></p>");
     $(".question").append(categories[category][i].question);
     $(".question").append("<p></p>");
